ef19a168b0338c66ca1e1acecadd30e0a58e2353
Aleksey <badproper98@gmail.com>
2022-11-11 10:40:57 +0300

    вынес все в гланвый файл, добавил выборку данных

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"html/template"
 	"net/http"
 
-	"github.com/dartweydr-wq/project_go/pkg"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type User struct {
@@ -30,5 +32,35 @@ func handleReq() {
 
 func main() {
 	//handleReq()
-	pkg.ConnectionDb()
+
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:8889)/example_go")
+	defer db.Close()
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Подключение к бд успешно")
+
+	// добавление данных
+	// insert, err := db.Query("INSERT INTO users (name, age) VALUES('Иван', 13),('Александр', 10),('Дмитрий', 22)")
+	// defer insert.Close()
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("Данные записались в бд")
+
+	// выборка данных
+	res, err := db.Query("SELECT name, age FROM users ")
+	defer res.Close()
+
+	if err != nil {
+		panic(err)
+	}
+
+	for res.Next() {
+		var user User
+		err = res.Scan(&user.Name, &user.Age)
+	}
+
 }
