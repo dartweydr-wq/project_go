4a8724b9feef1227e1b7dfeff6a866ced1b31dff
Aleksey <badproper98@gmail.com>
2022-11-27 13:02:59 +0300

    добавил обработку добавления данных в бд

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
@@ -33,9 +34,37 @@ func create(w http.ResponseWriter, r *http.Request) {
 	tmpl.ExecuteTemplate(w, "create", nil)
 }
 
+func save_article(w http.ResponseWriter, r *http.Request) {
+	title := r.FormValue("title")
+	anons := r.FormValue("anons")
+	full_text := r.FormValue("full_text")
+
+	if title == "" || anons == "" || full_text == "" {
+		fmt.Fprintf(w, "Не все данные заполнены")
+	} else {
+		db, err := sql.Open("mysql", "root:root@tcp(localhost:8889)/example_go")
+		defer db.Close()
+
+		if err != nil {
+			panic(err)
+		}
+
+		// добавление данных
+		insert, err := db.Query(fmt.Sprintf("INSERT INTO articles (title, anons, full_text) VALUES('%s','%s','%s')", title, anons, full_text))
+		defer insert.Close()
+
+		if err != nil {
+			panic(err)
+		}
+
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+}
+
 func handleReq() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/create", create)
+	http.HandleFunc("/save_article", save_article)
 	http.ListenAndServe(":8080", nil)
 }
 
